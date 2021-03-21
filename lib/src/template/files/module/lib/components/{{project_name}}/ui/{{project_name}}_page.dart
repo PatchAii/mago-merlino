c823e1975fb6e2e695d1b7c363d33a121dbdbb60
dbbd59 <davide.bolzoni@patchai.io>
2021-03-21 15:18:09 +0100

    updates bundle

--- lib/src/template/files/module/lib/components/{{project_name}}/ui/{{project_name}}_page.dart
+++ lib/src/template/files/module/lib/components/{{project_name}}/ui/{{project_name}}_page.dart
@@ -3,7 +3,6 @@ import 'package:components/ui/widgets/common/top_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
-import 'package:theme/theme_fields.dart';
 
 class {{project_name_capitalize}}Page extends StatelessWidget {
   const {{project_name_capitalize}}Page({
